8463910fa73fbd72b53b83841721f396dd5a332f
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2022-02-24 10:10:47 +0100

    setup glium tutorial module and stage 1 with Escape handling.

---
+++ src/glium_book/mod.rs
@@ -0,0 +1,21 @@
+#![allow(unused_imports)]
+use std::io::stdout;
+use crossterm::{
+    execute,
+    style::Print,
+};
+
+mod stage1;
+
+const LATEST_COMPLETED_STAGE: usize = 0;
+
+pub const GLIUM: &str = "glium";
+
+pub fn entrypoint(stage: Option<usize>) {
+    if stage.is_none() {
+        execute!(stdout(), Print(format!("defaulting to latest completed stage: {}", LATEST_COMPLETED_STAGE))).ok();
+        stage1::run();
+    } else {
+        execute!(stdout(), Print(format!("doing glium tutorial stage {}! :D", stage.unwrap()))).ok();
+    }
+}
---
+++ src/glium_book/stage1.rs
@@ -0,0 +1,37 @@
+use crossterm::event::KeyCode;
+use glium::glutin::{self, event::{KeyboardInput, VirtualKeyCode}};
+
+pub fn run() {
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let window_builder = glutin::window::WindowBuilder::new();
+    let context_builder = glutin::ContextBuilder::new();
+    let display = glium::Display::new(
+        window_builder,
+        context_builder,
+        &event_loop
+    ).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        match event {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                glutin::event::WindowEvent::KeyboardInput {
+                    input: KeyboardInput { virtual_keycode: Some(virtual_code), state, .. },
+                    ..
+                } => match (virtual_code, state) {
+                    (VirtualKeyCode::Escape, _) => *control_flow = glutin::event_loop::ControlFlow::Exit,
+                    _ => return,
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
+}
