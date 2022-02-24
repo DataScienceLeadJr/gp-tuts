d925fb2a87895e2b5c807c2ffc6cc3f6b2c33b70
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2022-02-24 10:10:18 +0100

    setup init structure for running different tutorials and such.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+
+use clap::Parser;
+use crossterm::{
+    ExecutableCommand,
+    terminal::{
+        Clear,
+        ClearType,
+    },
+    cursor,
+};
+
+mod glium_book;
+
+/// Collective main entrypoint for running different graphics programming tutorial stages
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Arg {
+    /// Which tutorial to run.
+    #[clap(short, long)]
+    tutorial: String,
+
+    /// identify stage to run in given tutorial.
+    /// defaults to latest completed stage for given tutorial.
+    #[clap(short, long)]
+    stage: Option<usize>
+}
+
+fn main() -> crossterm::Result<()>{
+    std::io::stdout().execute(Clear(ClearType::All))?;
+    std::io::stdout().execute(cursor::MoveTo(0,0))?;
+
+    let args = Arg::parse();
+
+    match args.tutorial.to_lowercase().as_str() {
+        glium_book::GLIUM => glium_book::entrypoint(args.stage),
+        _ => panic!("That's not even a tutorial! For God's Sake!..")
+    };
+
+    Ok(())
 }
