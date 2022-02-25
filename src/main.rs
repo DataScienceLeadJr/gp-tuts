ed3145de2d395b76943fa6d412b812ed3b5ff520
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2022-02-25 17:09:33 +0100

    first working noise img.

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,26 @@ fn main() -> crossterm::Result<()>{
     std::io::stdout().execute(cursor::MoveTo(0,0))?;
 
     // TODOING: getting perlin noise up and running in this his-hey!
-    
+
+    use bracket_noise::prelude::*;
+
+    let mut noise = FastNoise::seeded(21);
+    noise.set_noise_type(NoiseType::PerlinFractal);
+    noise.set_fractal_type(FractalType::FBM);
+    noise.set_fractal_octaves(5);
+    noise.set_fractal_gain(0.6);
+    noise.set_fractal_lacunarity(2.0);
+    noise.set_frequency(2.0);
+
+    let wh = 250;
+    let mut img = image::GrayAlphaImage::new(wh, wh);
+    for x in 0..wh {
+        for y in 0..wh {
+            img.put_pixel(x, y, image::LumaA([255, (255.0 * noise.get_noise((x as f32) / 160.0, (y as f32) / 100.0)) as u8]));
+        }
+    }
+
+    img.save(".\\test.png").expect("failed to save to file.");
 
     // let args = Arg::parse();
 
