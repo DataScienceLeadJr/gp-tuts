e5d9c5976d7b932015c01588d70e54115d692679
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2022-02-25 10:22:08 +0100

    finish stage 6, with a combination of texture and interpolated coloring.

--- src/glium_book/stage6.rs
+++ src/glium_book/stage6.rs
@@ -4,30 +4,62 @@ use crossterm::event::KeyCode;
 use glium::{glutin::{
     self,
     event::{KeyboardInput, VirtualKeyCode}
-}, Program, Display, backend::Facade, uniform};
+}, Program, Display, backend::Facade, uniform, implement_vertex};
 use glium::Surface;
 
-use super::stage2::{
-    first_triangle,
-    buffer_a_shape,
-    dummy_marker,
-    Vertex,
-};
+use crate::glium_book::stage2::{buffer_a_shape, dummy_marker};
+
+#[derive(Copy, Clone)]
+pub struct Vertex {
+    position: [f32; 2],
+    texture_coordinates: [f32; 2],
+}
+
+implement_vertex!(Vertex, position, texture_coordinates);
+
+
+pub fn texture_triangle() -> Vec<Vertex> {
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+        texture_coordinates: [0.0, 0.0],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+        texture_coordinates: [0.0, 1.0],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+        texture_coordinates: [1.0, 0.0],
+    };
+
+    vec![
+        vertex1,
+        vertex2,
+        vertex3,
+    ]
+}
+
 
 pub fn vertex_shader_src() -> &'static str {
     r#"
         #version 140
 
         in vec2 position;
+        in vec2 texture_coordinates; // THIS!
 
         out vec2 my_attr; // out variables are communication channels from vertex to fragment shader programs!
+        out vec2 v_tex_coords; // OUT TO THIS!
 
         uniform mat4 translation_matrix; // UNIFORM meaning: global variable whose value is set for a draw call. aka. draw-context const variable.
         uniform mat4 rotation_matrix;
 
         void main() {
+            // Need to do this so that it can be "passed along" to the fragment shader so that the pixel-in-texture-position can be interpolated from the vertex pos.
+            v_tex_coords = texture_coordinates;
+
             // NOTE: the interpolation happens because the default for variables like this is the "smooth" setting.
             my_attr = position; // this is only the vertex position, but when the fragment shader reads it it is an interpolated value corresponding automatically to the pixels relative position! :D <3 
+
             vec4 rot_pos = rotation_matrix * vec4(position, 0.0, 1.0); // order is important, remember 4x4 * 4x1 vs 4x1 * 4x4
             gl_Position = translation_matrix * rot_pos; // order is important, remember 4x4 * 4x1 vs 4x1 * 4x4
         }
@@ -41,11 +73,20 @@ pub fn fragment_shader_src() -> &'static str {
         // like the rest: it just needs to be the same as in the vertex shader.
         in vec2 my_attr; // HERE IT IS :D the interpolated fragment position the vertex shader stored for us.
 
+        in vec2 v_tex_coords; // THIS COMING IN RIGHT HERE!
+
         out vec4 color;
 
+        uniform sampler2D tex;
+
         void main() {
-            // moving the color spectrum up above 0 to remove the black third.
-            vec2 c = (my_attr + 0.5001) * 0.45;
+            // getting the sampled interpolated pixel attached to the corresponding position within the texture (image)
+            vec4 c_tex = texture(tex, v_tex_coords);
+
+            // moving the color spectrum up above 0 to remove the black third for the attrib.
+            vec2 c_attrib = (my_attr + 0.5001) * 0.45;
+
+            vec2 c = c_tex.rg + (c_attrib * 0.1);
             // this just sets x to red and y to green, blue to 0.0 and opacity to 1.0
             color = vec4(c, 0.0, 1.0); // awesome
         }
@@ -66,9 +107,21 @@ pub fn run() {
         &event_loop
     ).unwrap();
 
+    use std::io::Cursor;
+    use image::load;
+
+    let image = load(Cursor::new(&include_bytes!("D:\\Projects\\Rust\\gp-tuts\\assets\\textures\\hamster.jpg")),
+                        image::ImageFormat::Jpeg).unwrap().to_rgb8();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgb_reversed(&image.into_raw(), image_dimensions);
+
+    let texture = glium::texture::texture2d::Texture2d::new(&display, image).unwrap();
+
+
+
     let mut t: f32 = -0.5;
-    let triangle = first_triangle();
-    let vertex_buffer = buffer_a_shape(&display, &triangle);
+    let triangle = texture_triangle();
+    let vertex_buffer = glium::vertex::VertexBuffer::new(&display, &triangle[..]).unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         match event {
@@ -112,7 +165,8 @@ pub fn run() {
                 [-t.sin(), t.cos() * 1.54, 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
                 [0.0, 0.0, 0.0, 1.0f32],
-            ]
+            ],
+            tex: &texture,
         };
         
         let mut frame = display.draw();
